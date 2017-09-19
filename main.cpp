584b165f98e02a8df75cbf56ff202e4a66c2a0b3
madscout <zkoopmans@gmail.com>
2017-09-18 23:27:13 -0500

    Afer 156

--- main.cpp
+++ main.cpp
@@ -201,14 +201,31 @@ int getWindowSize(int *rows, int *cols){
 
 /*** syntax highlighting ***/
 
+int is_separator(int c){
+    return isspace(c) || c == '\0' || strchr(",.()+-/*=~%<>[];", c) != NULL;
+}
+
 void editorUpdateSyntax(erow *row){
     row->hl = (unsigned char*)realloc(row->hl, row->rsize);
     memset(row->hl,HL_NORMAL, row->size);
 
-    for(int i = 0; i < row->rsize; i++){
-        if(isdigit(row->render[i])){
+    int prev_sep = 1;
+
+    int i = 0;
+    while(i < row->rsize){
+        char c = row->render[i];
+        unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+        if((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
+                (c == '.' && prev_hl == HL_NUMBER)){
             row->hl[i] = HL_NUMBER;
+            i++;
+            prev_sep = 0;
+            continue;
         }
+
+        prev_sep = is_separator(c);
+        i++;
     }
 }
 
@@ -451,6 +468,16 @@ void editorSave(){
 void editorFindCallback(char *query, int key) {
     static int last_match = -1;
     static int direction = 1;
+
+    static int saved_hl_line;
+    static char *saved_hl = NULL;
+
+    if(saved_hl){
+        memcpy(E.row[saved_hl_line].hl, saved_hl, E.row[saved_hl_line].rsize);
+        free(saved_hl);
+        saved_hl = NULL;
+    }
+
     if (key == '\r' || key == '\x1b') {
         last_match = -1;
         direction = 1;
@@ -477,6 +504,10 @@ void editorFindCallback(char *query, int key) {
             E.cy = current;
             E.cx = editorRowRxToCx(row, match - row->render);
             E.rowoff = E.numrows;
+
+            saved_hl_line = current;
+            saved_hl = (char *) malloc(row->rsize);
+            memcpy(saved_hl, row->hl, row->rsize);
 
             memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
             break;
